210bccc4327cff1dbbbe8d307acd6743d0af15cc
Jhonatan <jfernandez@sigcomt.com>
2017-09-22 17:11:16 -0500

    ajustes

--- Source/Base.Web/Controllers/ReporteController.cs
+++ Source/Base.Web/Controllers/ReporteController.cs
@@ -1,20 +1,5 @@
-﻿using Base.BusinessEntity;
-using Base.BusinessLogic;
-using Base.Common;
-using Base.Common.DataTable;
-using Base.DTO;
-using Base.DTO.AutoMapper;
-using Base.Web.ApiService;
-using Base.Web.Core;
-using Base.Web.Models;
-using Base.Web.Utilities;
-using Newtonsoft.Json;
-using Stimulsoft.Report;
-using Stimulsoft.Report.Mvc;
-using System;
+﻿using Base.Web.Core;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Base.Web.Controllers
